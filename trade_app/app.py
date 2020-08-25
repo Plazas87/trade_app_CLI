08a1e0f85e56f2eba1f4b1aa268e64471be7e123
Andres <acpr87@gmail.com>
2020-08-25 12:58:29 +0200

    Clean code.

--- trade_app/app.py
+++ trade_app/app.py
@@ -158,12 +158,4 @@ class Controller:
 
 
 if __name__ == '__main__':
-    from .orders import BuyOrder
-
-    controller = Controller(1000, 0.005, 0.03, 0.3)
-    controller.run()
-    print(controller.portfolio.capital)
-    print(controller.trader.max_buy_per_trade)
-    buy_order = BuyOrder('NFLX', buy_price=10, quantity=10)
-    controller.open_position(buy_order)
-    print(controller.portfolio.capital)
+    pass
